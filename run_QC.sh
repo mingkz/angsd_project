520f0c3d1f263bc6e1a2f4acfcda1ac2ba124a65
Mingxuan Zhang <miz4003@curie.pbtech>
2021-03-21 20:52:59 -0400

    New feature count

--- run_QC.sh
+++ run_QC.sh
@@ -7,6 +7,7 @@ for SAMPLE in SL_1 SL_2 SL_3 TP_1 TP_2 TP_3
 do
 java -Xmx4G -jar ${QORTS_LOC}/bin/QoRTs.jar QC --singleEnded --generatePdfReport \
    ./bam/${SAMPLE}.bam \
-   /home/nib4003/ANGSD_2021_hw/final_project/gencode.vM25.primary_assembly.annotation.gtf \
+   --stranded \
+   ./gencode.vM11.annotation.gtf \
    $SAMPLE
 done
--- run_feature_count.sh
+++ run_feature_count.sh
@@ -2,6 +2,6 @@
 
 spack load subread
 input_files=$(ls ./bam/*.bam)
-gtf='/home/nib4003/ANGSD_2021_hw/final_project/gencode.vM25.primary_assembly.annotation.gtf'
+gtf='./gencode.vM11.annotation.gtf'
 
-featureCounts -a $gtf -f -O -F 'GTF' -g 'gene_id' $input_files -o fc_gene.txt
+featureCounts -a $gtf -f -T 20 -O -s 2 -F 'GTF' -g 'gene_id' $input_files -o fc_gene.txt
